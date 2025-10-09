54ea188ca8f3232fe349304fae6252444f663abe
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-10-08 20:51:49 -0600

    feat: Se implemento la entidad para los productos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,32 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+    name: 'products'
+})
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',{
+        unique: true,
+    })
+    title: string
+
+    @Column('float',{
+        default: 0
+    })
+    price: number
+
+    @Column({
+        type: 'text',
+        nullable: true,
+    })
+    description: string
+
+    @Column('int',{
+        default: 0,
+    })
+    stock: number
+    
+}
