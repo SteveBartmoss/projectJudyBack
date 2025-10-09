fb1e0604e69504ea5a4fed5d091598f2fabbc720
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-10-08 21:04:11 -0600

    feat: Se implemento el entity para los usuarios

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User {}
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('text',{
+        unique: true,
+    })
+    email: string
+
+    @Column('text')
+    password: string
+
+    @Column('text')
+    fullName: string
+
+    @Column('text',{
+        array: true,
+        default: []
+    })
+    roles: string[]
+
+    @BeforeInsert()
+    checkFieldsBeforeInsert(){
+        this.email = this.email.toLocaleLowerCase().trim()
+    }
+
+    @BeforeUpdate()
+    checkFieldsBeforeUpdate(){
+        this.checkFieldsBeforeInsert()   
+    }
+}
